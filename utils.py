3e8b2b5cfe20d262842faa3c7a327569d38ed811
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-04-19 16:34:56 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np 
 import pandas as pd
+from scipy.io import wavfile
 
 
-def makeIndex(filePath):
+def makeIndex(filePath: str):
     """ Indexes all files and extracts some metadata and the target. """
 
     data = list()
@@ -14,16 +15,28 @@ def makeIndex(filePath):
             nm, ext = os.path.splitext(filename)
 
             if ext.lower().endswith('.wav'):
-                
+
                 fullpath = os.path.join(os.path.abspath(root), filename)
                 rootPart = root.split('\\')
-                target   = rootPart[-1]                     # Normal / abnormal
-                machType = rootPart[-3]                     # Machine type
-                machId   = int(rootPart[-2].strip('id_'))   # Machine ID
-                noise    = rootPart[-4].strip(              # Background noise level
+                target   = rootPart[-1]                   # Normal / abnormal
+                machType = rootPart[-3]                   # Machine type
+                machId   = int(rootPart[-2].strip('id_')) # Machine ID
+                noise    = rootPart[-4].strip(            # Background noise level
                     '_'.join(machType))
 
                 data.append((fullpath, machId, machType, noise, target))
 
     cols = ['filepath', 'machine_id', 'machine_type', 'background_noise', 'target']
+
     return pd.DataFrame(data, columns = cols)
+
+
+def readWav(filepath: str, micID: int = None):
+    """ Reads a wav file recorded by microphone <micID>. If <micID> is 
+        not specified, it returns the recordings of all microphones
+    """
+
+    fs, data = wavfile.read(filepath) # Read file
+    if micID: data = data[:, micID]   # Return specific mic. record
+
+    return fs, data
