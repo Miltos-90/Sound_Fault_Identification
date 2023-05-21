e21235c02eb047a1f60208981d71450c6b904a2a
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-05-21 18:08:22 +0200

    Add files via upload

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,3 +1,14 @@
+"""
+This module implements the preprocessing steps mentioned in Section 2 of:
+
+"A large set of audio features for sound description (similarity and classification)
+in the CUIDADO project", Peeters G., 2004.
+
+URL: http://recherche.ircam.fr/anasyn/peeters/ARTICLES/Peeters_2003_cuidadoaudiofeatures.pdf
+(accessed 11/05/2023)
+"""
+
+
 from .array import chunk, detrend
 from . import spectra
 from .spectra import todb
@@ -5,4 +16,4 @@ from . import envelopes
 from . import filters
 
 
-from .preprocessing import findPeaks
+from .harmonic_model import getFundamentalFrequency
