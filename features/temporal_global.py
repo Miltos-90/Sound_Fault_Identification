3f5220c5b598fd5b927c91ba6967aeb00c61ff7c
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-06-21 16:27:15 +0200

    Add files via upload

--- features/temporal_global.py
+++ features/temporal_global.py
@@ -22,4 +22,49 @@ def features(frequencies: np.array, amplitudes: np.array, numHarmonics: int, axi
     hFreqs, _     = pre.harmonicModel(frequencies, amplitudesDB, pFreqs, numHarmonics, axis)
     f0            = np.take(hFreqs, [0], axis = axis) # 1st harmonic frequency = fundamental frequency
 
+    # If less than the requested number of peaks is found, matrices of a smaller size
+    # will be returned from the peakFinder() function. This might be problematic for bulk
+    # extraction of data (i.e. from multiple signals). This is why new arrays will be 
+    # created for the peak amplitudes and frequencies that will contain the expected
+    # number (= numHarmonics) of elements, filled with nans.
+    pFreqs = _padArray(pFreqs, numHarmonics, axis)
+    pAmps  = _padArray(pAmps,  numHarmonics, axis)
+
     return np.concatenate([maxAmplitude, pFreqs, pAmps, f0], axis = axis)
+
+
+def _padArray(
+    arr: np.array, numElements: int, axis: int, value: float = np.nan, direction: str = 'end'
+    ) -> np.array:
+    """ Pads an array along an axis over one direction with a constant value to achieve a specific
+        number of elements over that dimension.
+        Inputs:
+            arr  : Array to be padded
+            numElements: Required number of elements that the array should have after padding
+            axis : Axis along which the padding will take place
+            value: Value with which additional elements will be padded
+        
+        Outputs:
+            arr_: padded array
+    """
+    
+    # Evaluate the padwidth that is required to align the dimensions
+
+    padWidths = []
+    for dim in range(arr.ndim):
+        
+        if dim == axis:
+            add = numElements - arr.shape[axis]
+            if add < 0: raise ValueError('Negative pad width encountered.')
+            if   direction == 'start': padWidth = (add, 0) # insert elements to the start
+            elif direction == 'end'  : padWidth = (0, add) # append elements to the end
+            else: raise ValueError('Direction can be set to either "start" or "end".')
+        else:
+            padWidth = (0, 0)
+        
+        padWidths.append(padWidth)
+
+    # Make new array
+    arr_ = np.pad(arr, padWidths, 'constant', constant_values = value)
+    
+    return arr_
